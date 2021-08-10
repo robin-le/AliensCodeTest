9d8934e90631ca764b2ebc342e25f2a387a2a6bb
Robin <>
2021-08-10 15:09:47 +0700

    init project

---
+++ iOSCodeTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSCodeTest
+//
+//  Created by Robin Le on 8/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSCodeTest/iOSCodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCodeTestApp.swift
+//  iOSCodeTest
+//
+//  Created by Robin Le on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
